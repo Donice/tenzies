d118edc677f5fd64726d30253289309718eb0e05
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-09 03:44:16 +0100

    transfered abiliity to toggle isHold function to all die

--- src/App.js
+++ src/App.js
@@ -4,30 +4,43 @@ import { React, useState } from 'react';
 import uuid from 'react-uuid'
 
 function App() {
+  
   const allNewDice = () => {
     const newDice = []
 
     for(let i=0; i < 10; i++) {
       let randomNumber = {
         value: Math.ceil(Math.random() * 6), 
-        isHeld: true,
+        isHeld: false,
         id: uuid()
       }
       newDice.push(randomNumber)
-    }
-    
+    } 
     return newDice;
   }
-  console.log(allNewDice())
-
   
-  const [dice, setDice] = useState(allNewDice())
-
-  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} />)
+  const [dice, setDice] = useState(allNewDice()) // dice, through allNewDice() is an array of 10 numbers
 
   const rollDice = () => {
     setDice(allNewDice())
   }
+
+  const holdDice = (id) => {
+    setDice(oldDice => oldDice.map(die => { //oldDiice is a pseudo prop mirroring 'dice'
+      return die.id === id                  // It maps through all 10 numbers in the array, checking if the id, matched the id passed into the function
+        ? {...die, isHeld: !die.isHeld}     // if so, it swaps the id
+        : die                               // if not, it leaves the die
+    }))
+  }
+
+  const diceElements = dice.map(die => 
+    <Die 
+      key={die.id} 
+      value={die.value} 
+      isHeld={die.isHeld} 
+      holdDice={() => holdDice(die.id)}
+    />
+  )
 
   return (
     <main className="App">
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,10 +1,14 @@
 import '../App.css'
-const Die = ({ value, isHeld }) => {
+const Die = ({ value, isHeld, holdDice }) => {
     const styles = {
         backgroundColor: isHeld? "#59e391" : "white"
     }
     return (
-        <div className='die' style={styles}>
+        <div 
+            className='die' 
+            style={styles}
+            onClick={holdDice}
+        >
             <h1 className='die-font'>{value} </h1>
         </div>
     )
